8aac26b2ab7f348d68b7fb1c0f4fa08a0c304ef0
arlen02-01 <lcglcg0201@gmail.com>
2025-11-26 14:48:56 +0900

    test 3

--- src/main/java/com/arlen/ProjectOn/web/HomeController.java
+++ src/main/java/com/arlen/ProjectOn/web/HomeController.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    @GetMapping("/__version")
-    @ResponseBody
-    public String version() {
-        return "version-2025-11-26-01";
-    }
-    
     @GetMapping("/")
     public String home() {
         // templates/home.html 렌더링
